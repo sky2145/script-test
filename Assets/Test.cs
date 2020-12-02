9e4ace59ec71580db475a21359b6b7f9713d963c
sky2145 <74121357+sky2145@users.noreply.github.com>
2020-12-02 20:11:48 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        // 要素数5の配列を初期化する
+        int[] array = new int[5];
+
+        // 配列の各要素に値を代入する
+        array[0] = 10;
+        array[1] = 20;
+        array[2] = 60;
+        array[3] = 10;
+        array[4] = 70;
+
+        // 配列の要素をすべて表示する
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(array[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
---
+++ Assets/Tests.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //コンソールにHello、worldと表示
+        Debug.Log("Hello、world");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
